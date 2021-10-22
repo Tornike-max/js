12ffafd69b99e71b7d3838f481575832204144a4
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2021-10-23 02:37:39 +0400

    js file

---
+++ app.js
@@ -0,0 +1,35 @@
+var numVar = 20
+console.log(numVar)
+
+var stringManName = "Tornike"
+console.log(stringManName)
+
+var myFirstBoolean = (20 < 15)
+console.log(myFirstBoolean)
+
+myFirstBoolean = (20 > 10)
+console.log(myFirstBoolean)
+
+var myPerson = {
+    firstName: "Derrick",
+    lastName: "Rose",
+    age: 33,
+    Height: 192,
+    weight: 91,
+}
+console.log(myPerson)
+
+var basketBallPlayers = ["rose", "curry", "koby"];
+console.log(basketBallPlayers)
+
+var copperSword = 32;
+console.log(copperSword)
+
+var steelSword = 64;
+console.log(steelSword)
+
+var mithrilSword = 19;
+console.log(mithrilSword)
+
+var totalSwords = copperSword + steelSword + mithrilSword;
+console.log(totalSwords)
